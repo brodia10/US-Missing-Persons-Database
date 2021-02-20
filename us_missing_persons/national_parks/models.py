32e8bb7cc41191235941ceb6b7d2f93ddfd8ff98
brodia10 <brookrodia@gmail.com>
2021-02-20 15:55:41 -0500

    Configured National Parks for easy data import

--- us_missing_persons/national_parks/models.py
+++ us_missing_persons/national_parks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import truncatechars
 
 
 class NationalParks(models.Model):
@@ -6,27 +7,30 @@ class NationalParks(models.Model):
     This is the National Parks model.
     """
 
-    id = models.UUIDField(primary_key=True)
-    area_acres = models.FloatField()
-    area_square_km = models.FloatField()
-    coordinates_latitude = models.FloatField()
-    coordinates_longitude = models.FloatField()
-    date_established = models.DateField(auto_now=False, auto_now_add=False)
+    area_acres = models.CharField(null=True, blank=True, max_length=50)
+    area_square_km = models.CharField(null=True, blank=True, max_length=50)
+    coordinates_latitude = models.CharField(null=True, blank=True, max_length=50)
+    coordinates_longitude = models.CharField(null=True, blank=True, max_length=50)
+    date_established = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
     image_url = models.CharField(max_length=50)
     image_attribution = models.CharField(max_length=150)
     image_attribution_url = models.CharField(max_length=150)
     nps_link = models.CharField(max_length=150)
-    states_id = models.CharField(max_length=150)
     states_title = models.CharField(max_length=150)
     title = models.CharField(max_length=150)
-    park_id = models.CharField(max_length=150)
-    visitors = models.IntegerField()
-    world_heritage_site = models.BooleanField(null=True, blank=True)
+    visitors = models.CharField(max_length=50)
+    world_heritage_site = models.BooleanField(default=True)
 
     class Meta:
         verbose_name = "National Parks"
         verbose_name_plural = "National Parks"
 
     def __str__(self):
-        return self.title
+        """ Returns the string representation of National Parks. """
+        return f"{self.title} National Park"
+
+    @property
+    def short_description(self):
+        """ Truncates the description field to the specified length. """
+        return truncatechars(self.description, 100)
